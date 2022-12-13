f80a492e2d96aa3d1710a9b2c39bed2a3b2eb346
suresh <venkatasuresh.d@inndata.in>
2022-12-13 21:40:02 +0530

    post query from backend

--- index.js
+++ index.js
@@ -17,22 +17,29 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-// app.get("/api/get",(req,res)=>{
-//     const sqlGet = "SELECT * FROM userDetails";
-//     database.query(sqlGet,(error,result)=>{
-//         res.send(result)
-//     })
-// })
-
-
-app.get("/",(req,res)=>{
-    const sqlInsert = "INSERT INTO userDetails(name,email_id,password) VALUES('Rakesh','rakesh@gmail.com','rakesh123')";
-    database.query(sqlInsert,(error,result)=>{
-        console.log("error",error)
-        console.log("results",result)
-        res.send("hello express created successfully")
+app.get("/api/get",(req,res)=>{
+    const sqlGet = "SELECT * FROM userDetails";
+    database.query(sqlGet,(error,result)=>{
+        res.send(result)
     })
 })
+
+
+app.post("/api/register",(req,res)=>{
+   const userName =req.body.userName;
+   const userEmail = req.body.userEmail;
+   const userPassword = req.body.userPassword
+    const sqlInsert = "INSERT INTO userDetails(name,email_id,password) VALUES(?,?,?)";
+    database.query(sqlInsert,[userName,userEmail,userPassword],(error,result)=>{
+        if (error){
+            console.log(error)
+        }else{
+            res.send("you create successfully")
+        }
+    })
+})
+
+
 
 
 app.listen(5000,()=>{
