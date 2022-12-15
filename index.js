859ff086bbd3a4f0c6ccf42f6d3ab6cdb4c9d5a2
suresh <venkatasuresh.d@inndata.in>
2022-12-15 15:50:10 +0530

    api working fine

--- index.js
+++ index.js
@@ -16,10 +16,20 @@ const db = mysql.createPool({
 });
 
 
-app.use(cors())
+
 app.use(express.json())
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({extended:true}))
 
+//API for get all users
 app.get("/get/",(req,res)=>{
     const sqlGet = "SELECT * FROM userDetails";
     db.query(sqlGet,(error,result)=>{
@@ -29,62 +39,65 @@ app.get("/get/",(req,res)=>{
 })
 
 
-
-app.post("/register/",async(req,res)=>{
+//API for user register
+app.post("/register", async(req,res)=>{
   const {userName,userEmail,userPassword} = req.body
   const hashedPassword = await bcrypt.hash(userPassword,10)
   const selectUserQuery = "SELECT * FROM userDetails WHERE email_id = ?";
-
   db.query(selectUserQuery,[userEmail],(err,result)=>{
-   if (err){
-    return res.send({err:err});
-   }
-   if(result.length > 0){
-    return res.send("all ready exists ")
-   }else{
-        const sqlInsert = "INSERT INTO userDetails(name,email_id,password) VALUES(?,?,?)";
-        db.query(sqlInsert,[userName,userEmail,hashedPassword],(err,result)=>{
-            res.send("User create successfully")
-            if (err){
-               return res.send({err:err})
+    if (err) {
+          console.log({err:err});
+         }
+    if(result.length > 0){
+        
+          res.send("all ready exists")
+        }else{
+          const sqlInsert = "INSERT INTO userDetails(name,email_id,password) VALUES(?,?,?)";
+          db.query(sqlInsert,[userName,userEmail,hashedPassword],(err,result)=>{
+            if(err){
+              res.send({err:err})
             }else{
-                return res.send("User created successfully")
-            }
-        })
-    }
-})
-})
-
-
-
-
-  app.post("/login", (req, res) => {
-    const {userName,userPassword} = req.body
-    const SelectUser = "SELECT * FROM userDetails WHERE name = ?";
-  
-    db.query(SelectUser,[userName],(err, result) => {
-        if (err) {
-          res.send({ err: err });
-        }
-  
-        if (result.length > 0) {
-          bcrypt.compare(userPassword, result[0].password, (error, response) => {
-            if (response) {
-            //   req.session.user = result;
-            //   console.log(req.session.user);
-              res.send({jwt_token:result[0].password});
-            } else {
-              res.send({ message: "Wrong username/password combination!" });
+              res.send("User created successfully")
             }
           });
-        } else {
-          res.send({ message: "User doesn't exist" });
+
+
+
+
         }
-      }
-    );
   });
-  
-  
+
+
+})
+
+//API for login
+app.post("/login", (req, res) => {
+  const {userName,userPassword} = req.body
+  const SelectUser = "SELECT * FROM userDetails WHERE name = ?";
+
+  db.query(SelectUser,[userName],(err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        bcrypt.compare(userPassword, result[0].password, (error, response) => {
+          if (response) {
+          //   req.session.user = result;
+          //   console.log(req.session.user);
+            res.send({jwt_token:result[0].password});
+          } else {
+            res.send({ message: "Wrong username/password combination!" });
+          }
+        });
+      } else {
+        res.send({ message: "User doesn't exist" });
+      }
+    }
+  );
+});
+
+
 
 
 
